7ea1db7fa5924c46ecf1c7adfb11fe049d9f7adb
Areg2001 <107401750+Areg2001@users.noreply.github.com>
2022-06-16 09:27:48 +0400

    Update Exercise_1.7.py

--- Exercise_1.7.py
+++ Exercise_1.7.py
@@ -1,22 +1,17 @@
-def zero_degree_of_number(a,b):
-    return 1
-
-def degree_of_number(a, b):
-    return a ** b
-
-def degree_of_reversal(a, b):
-    return ((1/a) ** (-b))
-
 def pow(a, b):
-    if(b > 0):
-       return degree_of_number(a, b)
-    elif(b < 0):
-        return degree_of_reversal(a,b)
-    elif(b == 0):
-        return zero_degree_of_number(a,b)        
+    res,count = 1, 0
 
-print(pow(2,0))            
+    if b < 0:
+        a = 1/a
 
-"""We do not need to chek a == 0 and b < 0 case because Python
+    while count < abs(b):
+        res = res * a
+        count = count + 1
+
+    return res       
+
+print(pow(4,-3))            
+
+"""We do not need to check a == 0 and b < 0 case because Python
    will automatically give an ZeroDivisionError"""
 
