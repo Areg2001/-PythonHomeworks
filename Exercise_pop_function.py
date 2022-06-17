1b0dd7878d4abf1bdb8b27c58cce3c858b807e94
Areg2001 <107401750+Areg2001@users.noreply.github.com>
2022-06-17 12:17:16 +0400

    Update Exercise_pop_function.py

--- Exercise_pop_function.py
+++ Exercise_pop_function.py
@@ -3,8 +3,9 @@ lst = [3,4,5,6]
 def pop_(data, index):
 
     if index == None:
+        tmp = data[-1]
         del data[-1]
-        return data
+        return tmp
 
     tmp = data[index]
     del data[index]
